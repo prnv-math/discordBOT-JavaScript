e227b1faf1368827f0eb671e8a488508f9e78f12
p-r-o-m-e <97275566+p-r-o-m-e@users.noreply.github.com>
2022-02-04 09:40:38 -0800

    added RULES command, further added to /start

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const discord = require("discord.js");
 const config = require("./config.json");
 
+var jimp = require('jimp');
+
 const { MongoClient } = require('mongodb');
 const url = 'mongodb://localhost:27017';
 const dbClient = new MongoClient(url);
@@ -16,16 +18,6 @@ console.log("running code")
 ownerid = config.ownerid
 console.log("code ownerid : " + ownerid)
 
-let embedVar = new discord.MessageEmbed()
-
-function emb(t,desc="", col = 0x3AABC2) 
-{
-        embedVar.setTitle(t);
-        embedVar.setColor(col);
-        embedVar.setDescription(desc);
-
-        return embedVar;
-}
 
 
 	// .setColor('#0099ff')
@@ -56,29 +48,103 @@ async function dbmain()
   // the code examples can be pasted here...
 
   // const insertResult = await globalOBJ.collection.insertMany([{userid: 1 ,  gameid: 2 ,  status: 'ok' }]);
+ 
   // console.log('Inserted documents =>', insertResult);
-  
-  // let res = await globalOBJ.collection.find({userid:1}).toArray();
+
+  const r = await globalOBJ.collection.insertOne({userid : 0, gameid : 11121513, name : 'bot', });
+
+  let res = await globalOBJ.collection.find({userid:0}).toArray();
 
   // =============================================================
   // console.log(res);
-  // console.log(res[0]['gameid']);
+  console.log(res[0]);
+  res = await globalOBJ.collection.find({userid:ownerid}).toArray();
+  console.log(res[0]);
+  console.log("find owner gameid : " + res[0]['gameid']);
   // ============================================================
   
   return 'done.';
 
 }
 
+function deluser(uid) {
+  globalOBJ.collection.deleteOne( { userid : uid } );
+}
+// deluser(ownerid);
 
+async function getStatus(uid) {
+  console.log("whole collection : ");
+  globalOBJ.collection.find().toArray();
+  let res = await globalOBJ.collection.find({userid:uid}).toArray();
+  console.log("get status res : " + res[0]['status']);
+ 
+  try {
+    // globalOBJ.status = res[0]['status']; 
+    return res[0]['status'];
+  }
+  catch{
+    return 'no status';
+  }
+  // if (res[0] == undefined)
+  //   {return 0;}
+  // if (res[0]['status'] != undefined) 
+  // {
+  //   globalOBJ.status = res[0]['status']; 
+  //   return res[0]['status'];
+  // }
+  // else return 'x';
+}
 
 async function getGameid(uid) {
 
-  const res = await globalOBJ.collection.find({userid:uid}).toArray();
-  if (res[0]['gameid']) 
-  {
+  let res = await globalOBJ.collection.find({userid:uid}).toArray();
+  
+  try {
+    // globalOBJ.gameid = res[0]['gameid']; 
     return res[0]['gameid'];
   }
-  else return 0;
+  catch{
+    return -1;
+  }
+  // console.log(res[0] == undefined);
+  // // console.log(res[0]['gameid'] == undefined);
+  // if (res[0] == undefined){
+  //   return 0;
+  // }
+  // if (res[0]['gameid'] != undefined) 
+  // { 
+  //   globalOBJ.gameid = res[0]['gameid']; 
+  //   return res[0]['gameid'];
+  // }
+  // else return 0;
+}
+
+async function createUser(uid) {
+ const res = await globalOBJ.collection.find({userid:0}).toArray();
+ const g = res[0]['gameid'];
+ console.log("bot gameid : "+g);
+ if ((g + 1) < 99999999) 
+ {
+ const newENtry = await globalOBJ.collection.insertOne({userid: uid ,  gameid: g + 1 ,  status: 'agreed' });
+ const updated = await globalOBJ.collection.updateOne({userid:0}, {$set : {gameid : (g+2)}}); 
+ let res = await globalOBJ.collection.find({userid:uid}).toArray();
+
+ console.log('newUserid : ' + res[0]['gameid'] + " , newbotgameid : " + (g+2));
+//  console.log('new entry status :  '+newENtry[0]['status']);
+ globalOBJ.gameid = res[0]['gameid'];
+ console.log('globalobj newusergameid :' + globalOBJ.gameid);
+ globalOBJ.status = res[0]['status'];
+ }
+ else 
+ {
+   console.log('db gameid overflow');
+ }
+}
+
+function changeStatus(uid, s) {
+
+  globalOBJ.collection.updateOne({userid:uid}, {$set : {status : s}});
+  // const res = await globalOBJ.collection.find({userid:uid}).toArray();
 }
 
 client.on('ready', () => {
@@ -88,56 +154,126 @@ client.on('ready', () => {
 
 });
 
-// client.on("messageCreate", function(message) 
-// {
-//     if (message.author.bot) return;
-//     con = message.content;
-//     channel= message.channel;
-//     con = con.split(" ")
 
-//     if (con[0].toUpperCase() == prefix.toUpperCase()) 
-//     {
-//       con = con[1]
-//       if (con == "quit")
-//       {
-//         if (message.author.id = ownerid) 
-//         {
-//         channel.send("``` shutting down ```")
-//         setTimeout(() => { process.exit(); }, 2000);
-//         }
-//         else
-//         {
-//           message.reply("Nice try...")
-//         }
-//       }
-//       else if (con == "ping") 
-//         message.channel.send("pong!")
-//         // client.emit('interactionCreate', message);
-//       else if (con == "help") {
-//         let e = emb("**For more info:** ` "+prefix+" help [command] `", "**Add ` "+prefix+" ` before any command**");
-//         e.set_author("Commands", url="", icon_url="https://image.freepik.com/free-vector/blue-pencil-with-pixel-art-style_475147-504.jpg")
-        
-//         channel.send({ embeds: [e] })
-
-//       }
-
-
-//     }
-// }
 // );
 
 client.on('interactionCreate', async interaction => {
 	
+  let e = {};
+  const channel=interaction.channel;
+
+  function emb(t,desc="", col = 0x3AABC2) 
+  {
+          const embedVar = new discord.MessageEmbed();
+          embedVar.setTitle(t);
+          embedVar.setColor(col);
+          embedVar.setDescription(desc);
+  
+          return embedVar;
+  }
+  // ==============
+
+
+  
+  // ==============
+  async function store_avatars()
+  {
+    let f =  jimp.read('https://i.imgur.com/Z2LbWqO.jpg', (err, fem) => {
+      if (err) throw err;
+      fem
+        .resize(64, 64) // resize
+        .quality(60) // set JPEG quality
+         // set greyscale
+        .write('fimg.jpg'); // save
+     });
+
+    let f2 = jimp.read('https://i.imgur.com/9fuHTih.jpg', (err2, m) => {
+    if (err2) throw err2;
+    m
+      .resize(64, 64) // resize
+      .quality(60) // set JPEG quality
+       // set greyscale
+      .write('mimg.jpg'); // save
+   });
+   return 'done';
+  }
+  async function customizepfp() {
+    
+  //  await channel.send("> customize profile :");
+  //  let res = await store_avatars();
+   
+   let avatar = "";
+
+   const file = new discord.MessageAttachment('fimg.jpg');
+   
+   const file2 = new discord.MessageAttachment('mimg.jpg');
+
+
+   await channel.send({files : [file, file2]});
+   const m1 = await channel.send("> 1 : please select character" + " <@" + interaction.member.id + ">");
+   
+   const emo = ['♀️','♂️'];
+   try {
+     await m1.react(emo[0]);
+     await m1.react(emo[1]);
+   }catch (err) {
+       console.error('failed to add reaction [' + err + "]");
+   }
+
+   const filter = (reaction, user) => {
+      // console.log(reaction.emoji.name);
+      return emo.includes(reaction.emoji.name) && (user.id === interaction.member.id);
+   };
+    
+   m1.awaitReactions({ filter, max: 1, time: 10000, errors: ['time'] })
+    .then(async collected => {
+      const reaction = collected.first();
+
+      if (reaction.emoji.name === emo[0]) {
+        char = "female_teen";
+      }
+      else {
+        char = "male_teen";
+      }
+      m1.channel.send("`You reacted with "+char+"`");
+      
+      const m2 = await m1.channel.send(">>> 2 : please enter a username\n(use letters, digits, and underscore)\n`/action username <enter username>`");
+      const filter2 = response => {
+        return response.content.toLowerCase().startsWith('/action username');
+      };
+      m2.channel.awaitMessages({ filter2, max: 1, time: 30000, errors: ['time'] })
+        .then (collected => {
+          channel.send(`> ${collected.first().content.split(' ', 3)[2]} detected`);
+          const res = collected.first().content.split(' ', 3)[2];
+          console.log("username : " + res);
+        
+        
+        // ==============
+        })
+        .catch(collected => {
+          channel.send('`no response found`');
+        });
+      
+      // ==============
+    })
+    .catch(collected => {
+    m1.reply('`You reacted with neither emotes.`');
+    });
+  
+   
+  //  =============================
+  }
+
   if (!interaction.isCommand()) return;
   // console.log(interaction.toString())
   // console.log(interaction)
-  const channel=interaction.channel;
-  const checkusr = getGameid(interaction.member.id);
+  const checkusr = await getGameid(interaction.member.id);
+  console.log('checkuser = ' + checkusr);
   if ((interaction.commandName != 'start') && (interaction.commandName != 'help') && (interaction.member.id != ownerid))
   {
     // try {
       console.log("trigger user check in db");
-      if (checkusr == 0){
+      if (checkusr < 0){
         interaction.reply("You are a new face! Check out `"+prefix+" help` or `"+prefix+" start` <@" + interaction.member.id + ">");
         return;
       }
@@ -145,13 +281,7 @@ client.on('interactionCreate', async interaction => {
         console.log(checkusr + 'is existing user');
         
       }
-    // }
-    // catch{
-        //  await interaction.reply("You are a new face! Check out `"+prefix+" help` or `"+prefix+" start`")
-    //   return;
-    // }
   }
-
 	if (interaction.commandName === 'ping') {
 		await interaction.reply({ content: 'Pong!', ephemeral: 0 });
 	}
@@ -175,15 +305,12 @@ client.on('interactionCreate', async interaction => {
   }
   else if(interaction.commandName === 'help')
   {
-    let e=emb("**For more info:** ` "+prefix+" help [command] `", "**Add ` "+prefix+" ` before any command**");
-    // e.setAuthor("Commands","https://image.freepik.com/free-vector/blue-pencil-with-pixel-art-style_475147-504.jpg", "");
-    //await interaction.channel.send({ embeds: [embedVar] });
-    // console.log("\nEmbed !!!!!: \n"+e+"\n");
+    e=emb("**For more info:** ` "+prefix+" help [command] `", "**Add ` "+prefix+" ` before any command**");
     e.setAuthor(
       {
         name: 'Commands',
         url: '',
-        iconURL: 'https://i.imgur.com/D6u3P8Z.jpg'
+        iconURL: 'https://i.imgur.com/cweJrD0.png'
     }
     )
     function em(a, b, c="")
@@ -209,19 +336,40 @@ client.on('interactionCreate', async interaction => {
     await interaction.reply({ embeds: [e] });
     
   }
+  else if (interaction.commandName === 'rules')
+  {
+    e = emb("DISCO-LIFE : Rules", "`rules are necessary and understood regulations you must not defy in order to create a frinedly environment for everyone. Doing so will result in severe punishment, or a ban to make it a better place for other players.`")
+    // e.addField("#1 ", "In any social platform the game provides wihtin itself, you aren't compelled to be friendly, but do not be toxic , bully, intimidate, shame, insult, harass, troll, flame, cause upset or shock to another person, or exhibit vulgar , racist , or **any kind of hostile behavior,** towards other players in the game.");
+    // e.addField("#2 ", "Do not engage in **illegal things** within the game! including breaking Discord tos, using hacks, mods, cheats, automation software (commonly known as 'scripts', 'macros', or 'bots').");
+    // e.addField("#3 ", "Do not use resources gathered in the game for **'real money trading'.**");
+    // e.addField("#4 ", "You may not exploit **errors in design** ('bugs') or features which have not been documented to gain access which is otherwise not available or to gain an advantage over other Users, and You may not communicate any exploitable issues either directly or through public posting, to any other users of Disco'Life's Services")
+    // e.addField("#5 ", "While allowed to select a username for any item in the game, **do not use" +  " INAPPROPRIATE OR DISALLOWED NAMES.** the game RESERVES THE RIGHT TO REJECT ANY NAME IT CONCLUDES, IN ITS SOLE DISCRETION, IS OFFENSIVE, OBSCENE, OR THAT OTHERWISE VIOLATES THE NAMING POLICY FOR USERNAMES.".toLocaleLowerCase());
+    e.addField("\u200B"+"\n" + "\u200B" +"#1 ", "In any social platform the game provides wihtin itself, do not be toxic , bully, intimidate, shame, insult, harass, troll, flame, cause upset or shock to another person, or exhibit vulgar , racist , or __any kind of hostile behavior,__ towards other players in the game.");
+    e.addField("\u200B"+"\n" + "\u200B" +"#2 ", "Do not engage in __illegal things__ within the game! including breaking Discord ToS, using hacks, mods, cheats, automation software (commonly known as 'scripts', 'macros', or 'bots').");
+    e.addField("\u200B"+"\n" + "\u200B" +"#3 ", "Do not use resources gathered in the game for __'real money trading'.__");
+    e.addField("\u200B"+"\n" + "\u200B" +"#4 ", "You may not exploit __errors in design__ ('bugs') or features which have not been documented to gain access which is otherwise not available or to gain an advantage over other Users, and You may not communicate any exploitable issues either directly or through public posting, to any other users of Disco'Life's Services")
+    e.addField("\u200B"+"\n" + "\u200B" +"#5 ", "While allowed to select a username for any item in the game, __do not use" +  " INAPPROPRIATE OR DISALLOWED NAMES.__ the game RESERVES THE RIGHT TO REJECT ANY NAME IT CONCLUDES, IN ITS SOLE DISCRETION, IS OFFENSIVE, OBSCENE, OR THAT OTHERWISE VIOLATES THE NAMING POLICY FOR USERNAMES.".toLocaleLowerCase());
+    // let txt = "";
+    // for (let x in e) {
+    //   txt += e[x] + " ";
+    // };
+    // console.log (txt);
+    interaction.reply({ embeds: [e] });
+    e = {};
+  }
   else if(interaction.commandName === 'start')
   {
-    if (checkusr != 0) 
+    if (checkusr < 0) 
     {
-    const message = await interaction.reply({content: ">>> [ boots up ]\n\nYou want to play Disco-Life! \nCheck out gameplayinfo,\nMake sure you have read and\naccepted the rules .\nThen react with :thumbsup: !\n\n`"+prefix.toLowerCase()+" gameplayinfo`, `"+prefix.toLowerCase()+" rules`", fetchReply:true})
-    let emo = '👍';
+    const message = await interaction.reply({content: ">>> [ booting up ]\n\nYou want to play Disco-Life! \nCheck out gameplayinfo,\nMake sure you have read and\naccepted the rules .\nThen react with :thumbsup: !\n\n`"+prefix.toLowerCase()+" gameplayinfo`, `"+prefix.toLowerCase()+" rules`", fetchReply:true})
+    const emo = '👍';
     message.react(emo);
     // ==============================
     // wait for reaction thumbsup
      
     const filter = (reaction, user) => {
       // console.log(reaction.emoji.name);
-      return [emo].includes(reaction.emoji.name) && user.id === interaction.user.id;
+      return [emo].includes(reaction.emoji.name) && user.id === interaction.member.id;
     };
     
     message.awaitReactions({ filter, max: 1, time: 25000, errors: ['time'] })
@@ -229,8 +377,15 @@ client.on('interactionCreate', async interaction => {
       const reaction = collected.first();
   
       if (reaction.emoji.name === emo) {
-        channel.send('You reacted with a thumbs up.');
-        //RESUME   HERE
+        createUser(interaction.member.id);
+        let gid = getGameid(interaction.member.id);
+        channel.send("`please wait a second [backing up your data]`")
+        // setTimeout(() => { channel.send("`[ success ] your game id :" + '(' + globalOBJ.gameid +')`'); }, 7000);
+        // TIMEOUT WORKED AGAINST PROMISE. CONTINUE HERE
+        setTimeout(function() {
+          channel.send("`[ success ] your game id : " + '(' + globalOBJ.gameid +')`'); // runs first
+          customizepfp(interaction); // runs second
+        }, 3000)
       }
     })
     .catch(collected => {
@@ -242,14 +397,22 @@ client.on('interactionCreate', async interaction => {
     
     }
     else {
-      if(filteredDocs['status']  != 'customize')
+      let status = await getStatus(interaction.member.id);
+      let gid = await getGameid(interaction.member.id);
+      console.log('chk = ' +checkusr);
+      if(status != 'agreed')
+      {
+        channel.send( "`game id : ("+ gid +")`, " + '`status : ['+status +']`');
         interaction.reply("You already have an account.");
+      }
       else
       {
-        //customize profile , resume.
+        interaction.reply("`resuming profile customization \n(gameid : " + gid+ " )`")
+
+        customizepfp(interaction);
       }
     }
-}
+  }
 });
 
 
